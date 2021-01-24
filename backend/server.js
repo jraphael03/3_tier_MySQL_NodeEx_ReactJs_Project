b957a78e921d139e5cbb8074d1255f80748ac812
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-01-23 19:37:21 -0500

    connected MySQL to backend server, beginning process to connect clientside to serverside. read.txt for download info

---
+++ backend/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const { Router } = require('express');
+const cors = require('cors');
+
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(express.json()); // parses incoming requests with JSON payloads
+app.use(cors());        //allows clientside to make requests
+
+
+//create connection to database
+const db = mysql.createPool({
+  host: process.env.DB_HOST, //localhost
+  user: process.env.DB_USER, //root
+  password: process.env.DB_PASSWORD, //password
+  database: process.env.DB, //ravenbooks
+});
+
+// routes for database
+app.get("/questions", (req,res) => {                            //sends data to page
+    db.query("SELECT * FROM questions", (err, result) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post("/questions", (req, res) => {                         //sends data to the database
+    const insertQuery = "INSERT INTO questions SET ?";
+    db.query(insertQuery, req.body, (err, result) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.send("Question Added to Database")
+        }
+    })
+})
+
+app.put("/questions/:qid", (req, res) => {                          //put request for updating 
+    const updateQuery = 
+    "UPDATE question SET question = ?, cid SET cid = ?, WHERE qid = ?";
+    db.query(
+        updateQuery,
+        [req.body.question, req.body.cid, req.params.qid],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else{
+                res.send(result);
+            }
+        }
+    );
+});
+
+app.delete('/questions/:qid', (req, res) => {
+    db.query(
+        "DELETE FROM questions WHERE qid = ?",
+        req.params.qid,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    )
+})
+
+
+const listener = app.listen(process.env.PORT || 5000, () => {
+    console.log('App is listening on port ' + listener.address().port)
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <h1>3 tier project</h1>
+  );
+}
+
+export default App;
